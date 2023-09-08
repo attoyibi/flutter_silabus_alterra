9ce9c53f1a299f0fd6295572a05081506083803f
attoyibi <muhammadmuchson@gmail.com>
2023-09-08 19:37:24 +0700

    create task
    task 08 introduction flutter
    09 flutter clli
    muchson

--- muchson/lib/main.dart
+++ muchson/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// Failed to launch Pixel_3a_API_33_x86_64: Error: Emulator didn't connect within 60 seconds
+// flutter emulators --launch Pixel_3a_API_33_x86_64
 void main() {
   runApp(const HelloWord());
 }
@@ -10,8 +12,30 @@ class HelloWord extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AppBar(
-        title: const Text('Hai nama ku Muchson'),
+        // theme: ThemeData.dark(), // Mengatur tema gelap
+        // initialRoute: 'home',
+        // routes: {
+        //   'home': (_) => HomePage(),
+        // },
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Muchson App'),
+      ),
+      body: Center(
+        child: Text('material'),
+      ),
+    ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [Text('ini merupakan menu home page')],
       ),
     );
   }
